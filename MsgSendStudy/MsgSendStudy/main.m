2835bba6adcfb5e848e3b3156762b9c9635b7702
WTL <1294876219@qq.com>
2020-07-20 09:06:40 +0800

    init

---
+++ MsgSendStudy/MsgSendStudy/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MsgSendStudy
+//
+//  Created by 王天亮 on 2020/7/20.
+//  Copyright © 2020 王天亮. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
